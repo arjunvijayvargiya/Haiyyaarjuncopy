91ba49d21258add27a97716a03a08beea6f15d12
jarvis <arjun.vijay20@gmail.com>
2015-02-26 16:59:17 +0530

    first commit

---
+++ app/src/main/java/google/codeforindia/haiyya2/Contact.java
@@ -0,0 +1,10 @@
+package google.codeforindia.haiyya2;
+
+/**
+ * Created by Srujan Jha on 17-01-2015.
+ */
+public class Contact {
+    public int Id;
+    public String Name,Number;
+    public Double Lat,Lon;
+}
